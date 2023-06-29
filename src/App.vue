aa4ed1df01ce2780b7ac7c1f36fa77147d3cebf1
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2023-06-29 16:27:19 +0300

    обработка ответов

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,42 @@
       <v-col>
         <h3>Выберите тему</h3>
         <v-sheet>
-          <v-chip-group  active-class="deep-purple darken-1 white--text">
-            <v-chip 
-            class="deep-purple lighten-3"
-            v-for="item in this.$store.state.questions"
-             :key="item.name"
-              @click="selectTheme(item.onclick, $event)">
+          <v-chip-group active-class="deep-purple darken-1 white--text">
+            <v-chip
+              class="deep-purple lighten-3"
+              v-for="item in this.$store.state.questions"
+              :key="item.name"
+              @click="selectTheme(item.onclick, $event)"
+            >
               {{ item.name }}
             </v-chip>
           </v-chip-group>
         </v-sheet>
       </v-col>
     </v-row>
-    <vAnimals3d v-if=" this.$store.state.selectedTheme=='animals'"></vAnimals3d>
+    <vAnimals3d
+      v-if="this.$store.state.selectedTheme == 'animals'"
+    ></vAnimals3d>
   </v-app>
 </template>
 
 <script>
-import vAnimals3d from './components/v-animals3d.vue';
+import vAnimals3d from "./components/v-animals3d.vue";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    vAnimals3d
+    vAnimals3d,
     //
   },
 
-  data: () => ({
-
-  }),
+  data: () => ({}),
   methods: {
     selectTheme(theme, e) {
-      this.$store.commit('SET_SELECTED_THEME', theme);
-      this.$store.commit('SET_SELECT_Q', theme);
-    }
-  }
+      this.$store.commit("SET_SELECTED_THEME", theme);
+      this.$store.commit("SET_SELECT_Q", theme);
+    },
+  },
 };
 </script>
 <style>
@@ -46,7 +47,7 @@ export default {
   padding: 3rem;
 }
 
-.v-application--wrap{
+.v-application--wrap {
   min-height: auto;
 }
 </style>
